1a82c9150221cc08fef2f01cbf229247a4b2107f
federicoolivo <federicoolivo@hotmail.it>
2017-07-20 13:07:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+
+include('functions.php');
+
+//connect to database
+$con = connect_to_database();
+?>
+
+<HTML>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+<link rel="stylesheet" type="text/css" href="/style.css?v=<?=time();?>">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="../../myjs.js?v=<?=time();?>" ></script>
+<script src="jquery-3.2.0.min.js"></script>
+
+<div class="bgImage">
+
+<?php boardList(); ?>
+
+<!--BANNER-->
+<center>
+<?php
+$banner = "<A href = 'http://4kev.org/'><img src = '/banners/" . rand(0, 38) . ".gif' /></A>";
+echo $banner;
+?>
+
+<br><br>
+<table><td><center>
+<p style="font-size:30px;"><b>Welcome to 4kev</b></p>
+<?php echo $top_message; ?>
+</center><td></table>
+<br>
+<center><div style="width:50%">
+
+
+
+<!--LOGIN BAR-->
+<?php 
+if(!isset($_SESSION['ID']))
+    echo '<!--LOGIN BUTTON-->
+    <button id="showLogin" style="width:100px; text-align:center; height:30px;" onclick="showLogin()">Login</button>
+    <div id="login" style="display:none"><table>
+    <form action= "../login.php?x=' . $_SERVER['PHP_SELF'] . '" method="post" onsubmit="myButton.disabled = true; return true;">
+    <td><p>Email</p></td><td><input type="text" name="email" /></td>
+    <td><p>Password</p></td><td><input type="password" name="pwd" /></td>
+    <td><button type="submit" name="myButton">Log In</button></td>
+    </form></table><br></div>';
+else {
+    $sql = "SELECT * FROM users WHERE ID = " . $_SESSION['ID'];
+        $res = mysqli_query($con, $sql);
+        while($row = mysqli_fetch_assoc( $res ))
+             echo "
+    <table><td><p style='display:inline'>Logged in as <b>" . $row['name'] . "</b></p></td>";
+    echo '
+    <form action= "../logout.php?x=' . $_SERVER['PHP_SELF'] . '" method="post">
+    <td><button>Log Out</button></td>
+    </form></table>';
+}
+?>
+
+<!--ERROR / CONFIRMATION MESSAGE -->
+<?php
+    $err = $_GET['err'];
+    if($err == 1)
+        echo '<table><td style="height:30px; background:lightgreen; border:1px solid darkgreen; border-spacing: 1px; padding: 3px;"><p>You will receive an activation email</p></td></table>';
+    if($err == 2)
+        echo '<table><td style="height:30px; background:lightcoral; border:1px solid darkred; border-spacing: 1px; padding: 3px;"><p>Name is already taken</p></td></table>';
+    if($err == 3)
+        echo '<table><td style="height:30px; background:lightcoral; border:1px solid darkred; border-spacing: 1px; padding: 3px;"><p>Email is already registered</p></td></table>';
+    if($err == 4)
+        echo '<table><td style="height:30px; background:lightgreen; border:1px solid darkgreen; border-spacing: 1px; padding: 3px;"><p>Your account is now active</p></td></table>';
+    if($err == 5)
+        echo '<table><td style="height:30px; background:lightcoral; border:1px solid darkred; border-spacing: 1px; padding: 3px;"><p>Incorrect password. Try again</p></td></table>';
+    if($err == 6)
+        echo '<table><td style="height:30px; background:lightcoral; border:1px solid darkred; border-spacing: 1px; padding: 3px;"><p>Password is too short (min 8 characters)</p></td></table>';
+?>
+
+
+
+<!--REGISTER BUTTON-->
+<?php
+    if(!isset($_SESSION['ID']))
+        echo '<button id="showForm" style="width:100px; text-align:center; height:30px;" onclick="showForm()">Register</button>';
+?>
+
+<!--REGISTER WINDOW-->
+    <div id="form" style='display:none'>
+    <form action= "register.php" method="post" onsubmit="myButton.disabled = true; return true;">
+    <table>
+        <tr><td><input type="text" placeholder="Name" name="name" /></td></tr>
+        <tr><td><input type="password" placeholder="Password" name="pwd" /></td></tr>
+        <tr><td><input type="password" placeholder="Confirm password" name="pwd2" /></td></tr>
+        <tr><td><input type="text" placeholder="Email" name="email" /></td></tr>
+      
+        <tr><td><button style="text-align:center; width:100%;height:30px;" type="submit" name="myButton">Register</button></td></tr>
+    </table>
+    </form>
+    </div>
+
+</div>
+</center>
+
+<hr>
+
+</div>
+
+
+<div>
+<center>
+<table style="width: 50%">
+<?php
+//display last posts
+$sql = "SELECT * FROM posts ORDER BY ID DESC";
+$res = mysqli_query($con, $sql);
+echo "<tr><td style='text-align:center'><b><p>LAST POSTS</p></b></td></tr>";
+$cont = 0;
+while(($cont < 20) && $row = mysqli_fetch_assoc( $res )) {
+    if($row['board'] != "test") {
+
+        //stampa link to thread
+        if($row['replyTo'])
+            $num = $row['replyTo'];
+        else
+            $num = $row['ID'];
+        $threadlink = "http://4kev.org/threads/" . $num . ".php";
+
+        //stampa board
+        echo "<tr><td><p><a href='$threadlink'><b>No.{$row['ID']} {$row['board']}<br></b></a>";
+
+        //stampa commento
+            $rowComment = htmlspecialchars($row['commento']);
+        //divide line into words
+            $words = explode(" ", $rowComment);
+            foreach ($words as $word) {
+               $word = wordFilter($word);
+               echo "$word "; 
+            }
+
+        
+
+        echo "</p></td></tr>";
+        $cont++;
+    }
+}
+?>
+</td></table>
+</div>
+<hr>
+<div style="clear:both">
+<center>
+<p style="clear:both;"><i>kev@4kev.org</i> | <A href="http://4kev.org/rules.php">Rules</A>  | Visits: <?php include "count.php"; ?></p>
+</center>
+<br>
+</div>
+</center>
+</div>
+</HTML>
---
+++ template.php
@@ -0,0 +1,424 @@
+<?php
+session_start();
+include('../functions.php');
+
+//connect to database
+$con = connect_to_database();
+
+//get name of the board
+$currentPage =  preg_replace("/[^0-9]/","", basename($_SERVER['PHP_SELF'])); 
+$aa = "SELECT * FROM posts WHERE ID = $currentPage";
+$bb = (mysqli_query($con, $aa) );
+        while($row = mysqli_fetch_assoc( $bb )) 
+            $boardName = $row['board'];
+
+//check if user is a mod
+if( $_SESSION['ID'] == 11 || $_SESSION['ID'] == 6 || $_SESSION['ID'] == 7) 
+    $isMod = 1;
+
+     
+//prepare variables to insert
+
+//retrieve username
+if(isset($_SESSION['ID'])) {
+    $sql = "SELECT * FROM users WHERE ID = " . $_SESSION['ID'];
+    $res = mysqli_query($con, $sql);
+        while($row = mysqli_fetch_assoc( $res ))
+            $name = mysqli_real_escape_string($con, $row['name']);
+    }
+else {
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+}
+$options = mysqli_real_escape_string($con, $_POST['options']);
+$comm = mysqli_real_escape_string($con, $_POST['comment']);
+date_default_timezone_set('Europe/Paris');
+$date = date('d/m/Y H:i:s', time());
+$ipAddr = $_SERVER['REMOTE_ADDR'];
+$url = mysqli_real_escape_string($con, $_POST['url']);
+$image = basename($_FILES["fileToUpload"]["name"]);
+
+//if someone tries to post a malicious link, redirect him
+if (strpos($comm, 'href') !== false) {
+    header('Location: '.$_SERVER['PHP_SELF']);
+    die;
+}
+
+//delete post
+if($_GET['delete'] && $isMod == 1) {
+    $postDel = $_GET['delete'];
+    $del = $postDel . ".php";
+        unlink($del);
+    $sql = "DELETE FROM posts WHERE ID = $postDel OR replyTo = $postDel";
+    mysqli_query($con, $sql);
+    //register action in actionMod
+    $sql = "INSERT INTO actionMod (name, dateTime, ipAddress, action) VALUES ('$name','$date','$ipAddr','delete')";
+    mysqli_query($con,$sql);
+}
+
+//report post
+if($_GET['report']) {
+    $report = $_GET['report'];
+    $sql = "INSERT INTO reports (post, ipAddress) VALUES ('$report','$ipAddr')";
+    mysqli_query($con, $sql);
+}
+
+if(isset($_SESSION['ID'])) 
+  $loggedIn = 1;
+
+/*
+// ALLOW ONLY LOGGED IN USERS TO POST IMAGES
+if($image && !$loggedIn) {
+    header('Location: ' . $_SERVER['PHP_SELF'] . '?err=1');
+    die;
+}
+*/
+
+setcookie('keepName', $name, time()+3600, '/');
+setcookie('keepOptions', $options, time()+3600, '/');
+
+//check if bump limit is reached
+$x = "SELECT COUNT(*) AS replies FROM posts WHERE replyTo = $currentPage";
+$y = (mysqli_query($con, $x));
+$z = mysqli_fetch_assoc($y);
+$q = $z['replies'];
+$bumpLimit = 250;
+if($q >= $bumpLimit)
+    echo "<p><b>Bump limit exceeded. Can't post more replies.</b></p><br>";
+
+//check if user is banned
+if($comm || $url) {
+    $sql = "SELECT * FROM bannedUsers ";
+    $result = (mysqli_query($con, $sql));
+    while($row = mysqli_fetch_assoc( $result )) {
+        if($row['ipAddress'] == $ipAddr) {
+            //check if ban is expired
+            $actualDate = (float)date('YmdHis', time());
+            if($row['expire'] >= $actualDate) {
+                //if user is still banned, send him to ban page with query string with info about ban
+                $reason = $row['reason'];
+                $date1 = $row['date1'];
+                $date2 = $row['date2'];
+                header('Location: http://4kev.org/banned.php?reason=' . $reason . '&date1=' . $date1 . '&date2=' . $date2);
+                die;
+            }
+            
+        }
+    }
+}
+
+//insert data into table
+if(($comm || $url || $image) && ($q < $bumpLimit)) {
+
+//variables concerning image upload
+$oldName = basename($_FILES["fileToUpload"]["name"]);
+$imageFileType = pathinfo($oldName,PATHINFO_EXTENSION);
+if($oldName)
+    $newName = makeFileName() . "." . $imageFileType;
+$target_dir = "../uploads/";
+$target_file = $target_dir . $newName;
+$uploadOk = 1;
+
+    $sql = "INSERT INTO posts (name, options, commento, dateTime, replyTo, ipAddress, imageUrl, board, image, loggedIn) VALUES ('$name', '$options', '$comm', '$date', $currentPage, '$ipAddr', '$url', '$boardName', '$newName', '$loggedIn')";
+    mysqli_query($con, $sql);
+
+    //bump thread
+    if(($options != "sage") && ($options != "SAGE")) {
+        $selectSQL = "SELECT ID FROM posts ORDER BY ID DESC LIMIT 1;";
+        $result = (mysqli_query($con, $selectSQL) );
+        while($row = mysqli_fetch_assoc( $result )) 
+            $newBump = $row['ID'];
+        $updateSQL = "UPDATE posts SET bump=$newBump WHERE ID=$currentPage;";
+        $result = (mysqli_query($con, $updateSQL) );
+    }
+
+if($oldName) {
+    //insert image into database
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 2000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"  && $imageFileType != "JPG"  && $imageFileType != "PNG"  && $imageFileType != "JPEG"  && $imageFileType != "GIF") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            //echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+
+//redirect to same page
+header('Location: '.$_SERVER['PHP_SELF']);
+die;
+}
+
+?>
+
+<HTML>
+<head>
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+<link rel="stylesheet" type="text/css" href="/style.css?v=<?=time();?>">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="jquery-3.2.0.min.js"></script>
+<script type="text/javascript" src="../../myjs.js?v=<?=time();?>" ></script>
+
+<div class="bgImage">
+
+<?php //print a message if a post has been reported
+    if($_GET['report'])
+        echo "<script> alert('Reported'); </script>";
+?>
+
+<?php boardList(); ?>
+
+<!--BANNER-->
+<center>
+<?php
+$banner = "<A href = 'http://4kev.org/'><img src = '../banners/" . rand(0, 38) . ".gif' /></A>";
+echo $banner;
+?>
+
+<br><br>
+<table><td><center>
+<p style="font-size:30px"><b><? echo $boardName; ?></b></p>
+<?php echo $top_message; ?>
+</center><td></table>
+<br>
+
+<!--LOGIN BAR-->
+<?php 
+if(!isset($_SESSION['ID']))
+    echo '<!--LOGIN BUTTON-->
+    <button id="showLogin" style="width:100px; text-align:center; height:30px;" onclick="showLogin()">Login</button>
+    <div id="login" style="display:none"><table>
+    <form action= "../login.php?x=' . $_SERVER['PHP_SELF'] . '" method="post" onsubmit="myButton.disabled = true; return true;">
+    <td><p>Email</p></td><td><input type="text" name="email" /></td>
+    <td><p>Password</p></td><td><input type="password" name="pwd" /></td>
+    <td><button type="submit" name="myButton">Log In</button></td>
+    </form></table><br></div>';
+else {
+    $sql = "SELECT * FROM users WHERE ID = " . $_SESSION['ID'];
+        $res = mysqli_query($con, $sql);
+        while($row = mysqli_fetch_assoc( $res ))
+             echo "
+    <table><td><p style='display:inline'>Logged in as <b>" . $row['name'] . "</b></p></td>";
+    echo '
+    <form action= "../logout.php?x=' . $_SERVER['PHP_SELF'] . '" method="post">
+    <td><button>Log Out</button></td>
+    </form></table><br>';
+}
+?>
+
+<?php
+    $err = $_GET['err'];
+    if($err == 1)
+        echo '<table><td style="height:30px; background:lightcoral; border:1px solid darkred; border-spacing: 1px; padding: 3px;"><p>You must be logged in to upload images</p></td></table>';
+?>
+
+<!--POST REPLY BUTTON-->
+<button id="showForm" style="text-align:center; height:30px;" onclick="showForm()">Post a Reply</button>
+
+<!--submission form-->
+<!--il cancelletto significa che la form � submitted alla same page-->
+<div id="form" style="display:none">
+<form action="#" method="post" enctype="multipart/form-data" onsubmit="myButton.disabled = true; return true;">   
+<table>
+<tr><td><p>Name</p></td><td>
+<?php
+    if(isset($_SESSION['ID'])) {
+        $sql = "SELECT * FROM users WHERE ID = " . $_SESSION['ID'];
+        $res = mysqli_query($con, $sql);
+            while($row = mysqli_fetch_assoc( $res ))
+                echo '<b><p style="color:lawngreen">' . $row['name'] . "</p></b></td></tr>";
+    }
+    else
+        echo '<textarea rows="1" cols="30" input type="text" name="name" />' . $_COOKIE["keepName"] . '</textarea></td></tr>';
+?>
+<tr><td><p>Options</p></td><td><textarea style="vertical-align:middle" rows="1" cols="30" input type="text" name="options" /><?php echo $_COOKIE['keepOptions']; ?></textarea><input type="submit" value="Post" name="myButton" /></td></tr>
+<tr><td><p>Comment</p></td><td><textarea style="resize:both;" rows="4" cols="40" input type="text" name="comment" /></textarea></td></tr>
+<!--<tr><td><p>ImageURL</p></td><td><textarea rows="1" cols="40" input type="text" name="url" /></textarea></td></tr>-->
+<tr><td><p>File</p></td><td><input type="file" name="fileToUpload" id="fileToUpload"></td></tr>
+</table>
+</form>
+</div>
+<hr>
+</div>
+
+</center>
+
+<!--reply window-->
+<div id="draggable" class='replyWindow'>
+<table align='center' style="border-collapse:collapse; border: 1px solid fuchsia;">
+<th style="cursor:move;"><p>Post a reply<span class='close'>&times;</span></p></th>
+<form action='#' method='post' enctype="multipart/form-data" onsubmit="myButton.disabled = true; return true;">
+<tr><td>
+<?php
+    if(isset($_SESSION['ID'])) {
+        $sql = "SELECT * FROM users WHERE ID = " . $_SESSION['ID'];
+        $res = mysqli_query($con, $sql);
+            while($row = mysqli_fetch_assoc( $res ))
+                echo "<b><p style='color:lawngreen'>" . $row['name'] . "</p></b></td></tr>";
+    }
+    else
+        echo '<textarea placeholder="Name" rows="1" style="width: 300px" input type="text" name="name" />' . $_COOKIE["keepName"] . '</textarea></td></tr>';
+?>
+<tr><td><textarea placeholder="Options" rows="1" style="width: 300px" input type="text" name="options" /><?php echo $_COOKIE['keepOptions']; ?></textarea></td></tr>
+<tr><td><textarea id="linky" rows='4' style="width: 300px; resize:both;" input type='text' name='comment'></textarea></td></tr>
+<!--<tr><td><textarea placeholder="ImageUrl" rows="1" style="width: 300px" input type="text" name="url" /></textarea></td></tr>-->
+<tr><td><input type="file" style="display:inline" name="fileToUpload" id="fileToUpload"><input type='submit' align="right" name="myButton" /></td></tr>
+</form></table></div>
+
+<!--post preview-->
+<div class="preview">
+</div>
+     
+<?php
+//display posts
+$selectSQL = "SELECT * FROM posts ORDER BY ID ASC";
+$selectRes = mysqli_query($con, $selectSQL);
+
+while( $row = mysqli_fetch_assoc( $selectRes ) ){
+
+    //prepare variables
+    $rowImage = "../uploads/" . htmlspecialchars($row['image']);
+    $rowImage = str_replace("onerror","whatnow", $rowImage);  //protection against xss attack
+    $imageID = 'img' . $row['ID'];
+    $rowID = $row['ID'];
+    $rowName = htmlspecialchars($row['name']);
+    $rowSubject = htmlspecialchars($row['subject']);
+    $rowComment = htmlspecialchars($row['commento']);
+    $rowImageUrl = htmlspecialchars(addslashes($row['imageUrl']));  //protection against xss attack
+    $rowImageUrl = str_replace(" ","", $rowImageUrl);  //protection against xss attack
+    $rowImageUrl = str_replace("onerror","whatnow", $rowImageUrl);  //protection against xss attack
+    $rowImageUrl = str_replace("style","nope", $rowImageUrl);  //protection against xss attack
+    $idUrl = $rowID . 'a';
+
+    $space = str_repeat('&nbsp;', 2);  //spaces between picture and text
+     
+    if($row['ID'] == $currentPage || $row['replyTo'] == $currentPage) {
+    
+        if($row['ID'] == $currentPage || $row['replyTo'] == $currentPage)
+            echo nl2br("<div id='{$rowID}'  style='margin-bottom:5px;'><table style='display:inline-table;'><tr>");
+     
+        //show picture if present (URL)
+        if($row['imageUrl'])
+        echo "<td style='vertical-align:top'><img class='smallpic' id='{$idUrl}' src='$rowImageUrl'></td>";
+
+        //show picture if present (uploaded)
+        if($row['image'])
+        echo nl2br("<td style='vertical-align:top'><img class='smallpic' id=$imageID src='$rowImage' onclick='resizepic(this.id)'></td>");
+    
+        //print subject
+        echo "<td class='pad' style='vertical-align:top'><p style='display:inline;' class='grey'><b class='yellow'>$rowSubject</b>";
+    
+        if($row['loggedIn'] == 1)
+            echo nl2br(" <font color='orange'><b style='cursor:pointer;' title='Registered User'>&#9733 </b></font> ");
+
+        //select name color
+        if($row['options'] == 'xxxx')
+            echo nl2br("<font color='red'><b> ");
+        else if($row['options'] == 'xxxx')
+            echo nl2br("<font color='orange'><b> ");
+        else
+            echo nl2br("<font color='lawngreen'><b> ");
+     
+        //print anonymous if name is not present
+        if(!$row['name']) 
+            echo "Anonymous";
+    
+        //print name, date, time and post number
+        $hiddenButton = makeFileName();
+        echo "$rowName</b></font> {$row['dateTime']} No.<A class='quickReply'>{$row['ID']}</A> <a class='blue' onclick='showButton($hiddenButton)'>▶</a></p>";
+
+        //show delete button if user is a mod, else show report button
+        if($isMod == 1)
+            echo "  <form id='$hiddenButton' style='display:none' action='#' method='get'>
+                    <button type='submit' name='delete' value='{$row['ID']}'>Delete</button>
+                    </form>";
+        else
+            echo "  <form id='$hiddenButton' style='display:none' action='#' method='get'>
+                    <button type='submit' name='report' value='{$row['ID']}'>Report</button>
+                    </form>";
+
+//check if post is banned and echo message
+$sql2 = "SELECT * FROM bannedPosts";
+$res2 = mysqli_query($con, $sql2);
+while($row2 = mysqli_fetch_assoc($res2))
+    if($row['ID'] == $row2['post']) {
+        echo "<p style='color:red'><b>(User was banned for this post)</b></p>";
+        break;
+}
+
+        echo "<br><br>";
+
+        //PRINT COMMENT
+        //divide comment into lines
+        $lines = explode("\n", $rowComment);
+
+        //apply redtext
+        foreach ($lines as $line) {
+            //check for redtext
+            $checkRed = htmlspecialchars_decode($line);
+            if($checkRed[0] == '>')
+                echo nl2br("<p><font color='red'>");
+            else 
+                echo nl2br("<p><font>");
+
+        echo $space;
+    
+            //divide line into words
+            $words = explode(" ", $line);
+            foreach ($words as $word) {
+
+            $word = checkYoutube($word);
+            $word = wordFilter($word);
+    
+                //if word is a link to a post, show post preview
+                $checkLink = htmlspecialchars_decode($word);
+                if($checkLink[0] == '>' && $checkLink[1] == '>') {
+                    $postLink =  preg_replace("/[^0-9]/","", basename($word)); 
+                    $sql="SELECT * FROM posts WHERE ID = $postLink";
+                    $res = mysqli_query($con, $sql);
+                    while($row = mysqli_fetch_assoc( $res )) 
+                        $linkComm = htmlspecialchars(addslashes($row['commento']));
+                    $linkComm = htmlspecialchars(preg_replace("/\r\n|\r|\n/",'<br/>',$linkComm));
+                    echo nl2br("<A style='text-decoration: underline;' href='#$postLink' id={$postLink} class='postlink'>{$word} </A> ");
+                }
+               
+                //print original word
+                else
+                    echo "$word ";
+            }
+        echo nl2br("</font></p>");
+        }
+    echo '</td></tr></table></div>';
+    }
+}
+
+?>
+   
+<?php boardList(); ?>
+
+</html>
